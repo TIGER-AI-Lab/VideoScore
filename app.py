2361af333ed7f1a04970b65d96004e24ef7d2750
hexuan21 <hexuan21@mails.tsinghua.edu.cn>
2024-06-17 14:01:22 -0400

    update gh-pages

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import os
+import json
+import os
+from datetime import timedelta
+
+app = Flask(__name__,static_folder='/static/')
+app.secret_key = os.urandom(24)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=100)
+PORT_NUM = 7860
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,port=PORT_NUM)
